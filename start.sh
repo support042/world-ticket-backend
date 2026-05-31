be0a22e83f133e0705598aafe350efec192331a7
Nelson Okiki <nelson.okiki@kodehauz.com>
2026-05-31 21:19:19 +0100

    chore: limit celery worker concurrency to 1 to optimize memory usage

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Start the Celery worker in the background (&)
-celery -A app.infrastructure.celery_app worker --loglevel=info &
+# Start Celery in the background, limited to exactly 1 worker to save RAM!
+celery -A app.infrastructure.celery_app worker --loglevel=info --concurrency=1 &
 
-# Start the Web API in the foreground (keeps the container alive)
+# Start the FastAPI web server in the foreground
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
