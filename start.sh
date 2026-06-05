c812ba74e44f19800e6d8cb7198a668805cc9785
Nelson Okiki <nelson.okiki@kodehauz.com>
2026-06-05 09:17:12 +0100

    chore: remove Celery worker startup from start.sh to optimize resource usage

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# Start Celery in the background, limited to exactly 1 worker to save RAM!
-celery -A app.infrastructure.celery_app worker --loglevel=info --concurrency=1 &
-
 # Start the FastAPI web server in the foreground
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
